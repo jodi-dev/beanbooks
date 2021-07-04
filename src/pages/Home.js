6bdf1ec2f5092fc1ac6bb3264a9ec5f4fec65f40
Jodi <dqiao100@uottawa.ca>
2021-07-04 18:03:29 -0400

    title page done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,9 @@ import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
 import TimelineConnector from '@material-ui/lab/TimelineConnector';
 import TimelineContent from '@material-ui/lab/TimelineContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
+import TimelineOppositeContent from "@material-ui/lab/TimelineOppositeContent";
 import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -18,6 +20,10 @@ const useStyles = makeStyles((theme) => ({
       textAlign: 'center',
       color: theme.palette.text.secondary,
     },
+    oppositeContent: {
+        // TODO: adjust this value accordingly
+        flex: 0
+    }
   }));
 
 function Home() {
@@ -27,38 +33,77 @@ function Home() {
             <Grid item xs />
                 <Grid item container direction="row" justify="center" alignItems="center">
                     <Grid item sm={8} xs={12}>
-                        <h1>Hello</h1>
-                        <body>Lorem ipsum</body>
+                        <Box display="flex" justifyContent="center">
+                            <Box>
+                                <h1>Hello</h1>
+                            </Box>
+                        </Box>
+                        <Box display="flex" justifyContent="center">
+                            <Box>
+                                <body>
+                                    Create, find and share bean-sized stories written by users like yourself! This is a fun application that encourages the writer in everybody and helps you get over your writers block, get in the groove and get your daily writing practice in.
+                                </body>
+                            </Box>
+                        </Box>
                         <Grid container>
                             <Grid item md={6} xs={12}>
-                                <Timeline>
-                                    <TimelineItem>
-                                        <TimelineSeparator>
-                                        <TimelineDot />
-                                        <TimelineConnector />
-                                        </TimelineSeparator>
-                                        <TimelineContent>Eat</TimelineContent>
-                                    </TimelineItem>
-                                    <TimelineItem>
-                                        <TimelineSeparator>
-                                        <TimelineDot />
-                                        <TimelineConnector />
-                                        </TimelineSeparator>
-                                        <TimelineContent>Code</TimelineContent>
-                                    </TimelineItem>
-                                    <TimelineItem>
-                                        <TimelineSeparator>
-                                        <TimelineDot />
-                                        </TimelineSeparator>
-                                        <TimelineContent>Sleep</TimelineContent>
-                                    </TimelineItem>
-                                </Timeline>
+                                <Box justifyContent="flex-start">
+                                    <Timeline align="left">
+                                        <TimelineItem>
+                                        <TimelineOppositeContent
+                                            className={classes.oppositeContent}
+                                        />
+                                            <TimelineSeparator>
+                                            <TimelineDot />
+                                            <TimelineConnector />
+                                            </TimelineSeparator>
+                                            <TimelineContent>
+                                                <h2>Test</h2>
+                                                <body>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                                </body>
+                                            </TimelineContent>
+                                        </TimelineItem>
+                                        <TimelineItem>
+                                        <TimelineOppositeContent
+                                            className={classes.oppositeContent}
+                                        />
+                                            <TimelineSeparator>
+                                            <TimelineDot />
+                                            <TimelineConnector />
+                                            </TimelineSeparator>
+                                            <TimelineContent>
+                                                <h2>Test</h2>
+                                                <body>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                                </body>
+                                            </TimelineContent>
+                                        </TimelineItem>
+                                        <TimelineItem>
+                                        <TimelineOppositeContent
+                                            className={classes.oppositeContent}
+                                        />
+                                            <TimelineSeparator>
+                                            <TimelineDot />
+                                            </TimelineSeparator>
+                                            <TimelineContent>
+                                                <h2>Test</h2>
+                                                <body>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                                </body>
+                                            </TimelineContent>
+                                        </TimelineItem>
+                                    </Timeline>
+                                </Box>
                             </Grid>
                             <Grid item md={6} xs={12}>
                                 <h2>image</h2>
                             </Grid>
                         </Grid>
                     </Grid>
+                </Grid>
+                <Grid item container direction="row" justify="center" alignItems="center">
+                    Test
                 </Grid>
             <Grid item xs />
         </Grid>
--- src/pages/MainApp.js
+++ src/pages/MainApp.js
@@ -1,12 +1,32 @@
 import { Grid } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
 
 function MainApp() {
+    const classes = useStyles();
     return (
         <Grid item container>
             <Grid item xs />
                 <Grid item container direction="row" justify="center" alignItems="center">
                     <Grid item sm={8} xs={12}>
-                        <h2>Main App</h2>
+                        <h1>Main App</h1>
+                        <Grid>
+                            <Paper className={classes.paper}>Card 1</Paper>
+                        </Grid>
+                        <Grid>
+                            <Paper className={classes.paper}>Card 2</Paper>
+                        </Grid>
                     </Grid>
                 </Grid>
             <Grid item xs />
