df3c314546a7595a6b312bcd54d5daf697d40b02
Jodi <dqiao100@uottawa.ca>
2021-07-08 22:36:30 -0400

    made fab into speeddial

--- src/components/PromptFab.js
+++ src/components/PromptFab.js
@@ -3,23 +3,97 @@ import { makeStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import NavigationIcon from '@material-ui/icons/Navigation';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import SpeedDial from '@material-ui/lab/SpeedDial';
+import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon';
+import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    transform: 'translateZ(0px)',
+    flexGrow: 1,
+  },
+  exampleWrapper: {
+    position: 'relative',
+    marginTop: theme.spacing(3),
+    height: 100,
+  },
+  radioGroup: {
+    margin: theme.spacing(1, 0),
+  },
+  speedDial: {
+    position: 'absolute',
+    '&.MuiSpeedDial-directionUp, &.MuiSpeedDial-directionLeft': {
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+    },
+    '&.MuiSpeedDial-directionDown, &.MuiSpeedDial-directionRight': {
+      top: theme.spacing(2),
+      left: theme.spacing(2),
+    },
+    '&.MuiSpeedDial-fab': {
+      color: 'default',
+    },
+  },
   margin: {
     margin: theme.spacing(1),
   },
 }));
 
+const actions = [
+  { icon: <FavoriteIcon />, name: 'Like' },
+];
+
 export default function PromptFab() {
-    const classes = useStyles();
+  const classes = useStyles();
+  const [direction, setDirection] = React.useState('right');
+  const [open, setOpen] = React.useState(false);
+  const [hidden, setHidden] = React.useState(false);
+
+  const handleDirectionChange = (event) => {
+    setDirection(event.target.value);
+  };
+
+  const handleHiddenChange = (event) => {
+    setHidden(event.target.checked);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
   
-    return (
+  return (
+    <div className={classes.root}>
       <div>
-        <div>
-          <Fab size="small" color="primary" aria-label="add" className={classes.margin}>
-            <AddIcon/>
-          </Fab>
+        {/* <Fab size="small" color="primary" aria-label="add" className={classes.margin}>
+          <AddIcon/>
+        </Fab> */}
+          <div className={classes.exampleWrapper}>
+          <SpeedDial
+            ariaLabel="SpeedDial example"
+            className={classes.speedDial}
+            hidden={hidden}
+            icon={<SpeedDialIcon />}
+            onClose={handleClose}
+            onOpen={handleOpen}
+            open={open}
+            direction={direction}
+          >
+            {actions.map((action) => (
+              <SpeedDialAction
+                key={action.name}
+                icon={action.icon}
+                tooltipTitle={action.name}
+                onClick={handleClose}
+              />
+            ))}
+          </SpeedDial>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
