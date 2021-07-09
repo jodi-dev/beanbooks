64d125aba85bd054161f55ab946504cbe89867c0
Jodi <dqiao100@uottawa.ca>
2021-07-09 13:14:50 -0400

    added toggle switch

--- src/components/WriteBlock.js
+++ src/components/WriteBlock.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import { Grid } from '@material-ui/core';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { Grid, TextField, Button, Switch } from '@material-ui/core';
 import PromptFab from './PromptFab';
 import Box from '@material-ui/core/Box';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
@@ -34,6 +36,15 @@ const useStyles = makeStyles((theme) => ({
 
 function WriteBlock() {
     const classes = useStyles();
+
+    const [state, setState] = React.useState({
+        checkedB: true,
+    });
+    
+    const handleChange = (event) => {
+        setState({ ...state, [event.target.name]: event.target.checked });
+    };
+
     return (
         <div className={classes.root}>
             <Grid item container spacing={2}>
@@ -43,7 +54,7 @@ function WriteBlock() {
                             <Grid item xs={10}>
                                 <Box item>
                                     <body>
-                                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+                                        Whenever you speak, people hear you speaking in their native language. Most people are surprised and delighted. The cashier at McDonalds you’ve just talked to is horrified. “Nobody’s spoken that language in thousands of years.”
                                     </body>
                                 </Box>
                             </Grid>
@@ -62,12 +73,34 @@ function WriteBlock() {
                 <Grid item xs={12}>
                     <Paper className={classes.textPaper}>
                     <Grid item container>
+                        <Box p={2}>
+                            <TextField />
+                        </Box>
+                        <Box p={2}>
+                            <TextField />
+                        </Box>
                         <TextareaAutosize
                             maxRows={10}
                             aria-label="maximum height"
                             defaultValue="Write here!"
                             className={classes.textArea}
-                        />  
+                        /> 
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormGroup row>
+                            <FormControlLabel
+                                control={
+                                <Switch
+                                    checked={state.checkedB}
+                                    onChange={handleChange}
+                                    name="checkedB"
+                                    color="primary"
+                                />
+                                }
+                                label="Private"
+                            />
+                        </FormGroup>
+                        <Button variant="contained" color="primary">Finish</Button>
                     </Grid>
                     </Paper>
                 </Grid>
